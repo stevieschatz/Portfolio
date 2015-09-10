91e859dad1ff293f882cd1e2fa6a9307ba31203e
Steve <stevieschatz91@gmail.com>
2015-09-10 14:08:37 -0400

    Fix
    fix

--- MVCWithAngular/Models/BeerEditVM.cs
+++ MVCWithAngular/Models/BeerEditVM.cs
@@ -11,7 +11,7 @@ namespace MVCWithAngular.Models
 
         public int? Id { get; set; }
         public string Name { get; set; }
-        public String Colour { get; set; }
+        public string Colour { get; set; }
         public bool HasTried { get; set; }
 
     }
